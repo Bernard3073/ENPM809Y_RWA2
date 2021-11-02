6d22d0f1a01901597dbdbbf908cce1ab2ba3c2cc
Bernard3073 <bert41003@gmail.com>
2021-11-02 13:05:12 -0400

    use DFS for searching the maze

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include <array>
 
 #include "../include/mouse/mouse.h"
-
+#include "../include/api/api.h"
 
 int main() {
 	rwa2::Mouse mouse;
 	mouse.display_walls();
-
+	API::setColor(0, 0, 'G');
+	API::setText(0, 0, "abc");
 }
 
--- src/mouse.cpp
+++ src/mouse.cpp
@@ -2,6 +2,8 @@
 #include "../include/api/api.h"  
 #include "../include/util/util.h"
 #include <string>
+#include <stack>
+#include <utility>
 
 void rwa2::Mouse::display_walls() {
     for (int x = 0; x < m_maze_width; x += 1) {
@@ -29,4 +31,74 @@ void rwa2::Mouse::display_walls() {
             API::setText(x, y, std::to_string(m_maze.at(x).at(y).compute_number_of_walls()));
         }
     }
+}
+
+bool is_in_node_list(std::pair<int, int> &cur_node, std::vector<std::pair<int, int>> &node_list){
+    // use binary search to check if current node is in the list of nodes
+    int flag = 0;
+    int start = 0, end = node_list.size();
+    while(start < end && !flag){
+        int middle = start + (end - start)/2;
+        if(node_list[middle].first == cur_node.first \
+            && node_list[middle].second == cur_node.second) return true;
+        else if(node_list[middle].first > cur_node.first) end = middle - 1;
+        else start = middle + 1;
+    }
+    return false; 
+}
+
+bool rwa2::Mouse::search_maze(){
+    // current node
+    std::pair<int, int> cur_node = rwa2::Mouse::get_cur(); 
+    // goal node
+    std::pair<int, int> goal_node = rwa2::Mouse::get_goal();
+
+    // stack of nodes
+    std::stack<std::pair<int, int>> st;
+    // list of nodes
+    std::vector<std::pair<int, int>> node_list;
+    // current node is not the goal
+    if (!(cur_node.first == goal_node.first \
+        && cur_node.second == goal_node.second)){
+        if(!st.empty()) st.push(cur_node);
+    }
+    // current node is the goal
+    else return true;
+    if(!is_in_node_list(cur_node, node_list)) 
+        node_list.push_back(cur_node);
+    // check North
+    if(m_maze.at(cur_node.first).at(cur_node.second).is_wall(direction::NORTH)){
+        st.push(cur_node);
+        cur_node.second += 1;
+    }
+    // check East
+    else if(m_maze.at(cur_node.first).at(cur_node.second).is_wall(direction::EAST)){
+        st.push(cur_node);
+        cur_node.first += 1;
+    }
+    // check South
+    else if(m_maze.at(cur_node.first).at(cur_node.second).is_wall(direction::SOUTH)){
+        st.push(cur_node);
+        cur_node.second -= 1;
+    }
+    // check West
+    else if(m_maze.at(cur_node.first).at(cur_node.second).is_wall(direction::WEST)){
+        st.push(cur_node);
+        cur_node.first -= 1;
+    }
+    // no valid nodes: backtrack
+    else{
+        // remove current node from stack
+        if(!st.empty()) st.pop();
+        // empty stack = goal not found
+        else return false;
+    }
+    if(!st.empty()){
+        // current node is now the one at the top of stack
+        m_x = st.top().first;
+        m_y = st.top().second;
+        search_maze(); 
+    }
+    // empty stack = goal not found
+    else return false;
 }
